0dd6176ceac6225e96ae73cfaf35439bc1ce0761
chrisperk <christopherperkins.512@gmail.com>
2018-01-30 19:27:34 -0600

    added loading spinner on search

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,15 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Post from '../Post/Post';
 import './PostList.css';
+import loader from '../Post/images/loader.gif';
 
 const PostList = props => {
   return (
     <div className="list-wrapper">
       <main>
-        <ul>
+        <div className={props.searching ? 'loading-space show' : 'loading-space'}>
+          <img src={loader} alt="loading spinner" />
+        </div>
+        <ul className={props.searching ? 'hide' : ''}>
           {props.posts.map(post => {
             return (
-              <li key={post.data.id}>
+              <li
+                key={post.data.id}
+                onClick={() => props.handleSelectPost(post)}
+              >
                 <Post {...post.data} />
               </li>
             );
@@ -24,5 +31,7 @@ const PostList = props => {
 export default PostList;
 
 PostList.propTypes = {
-  posts: PropTypes.array
+  posts: PropTypes.array,
+  handleSelectPost: PropTypes.func,
+  searching: PropTypes.bool
 };
